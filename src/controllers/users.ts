44001a2466cc89cc734a938a4b9f836dee5265ee
Francoparodi01 <francoparodi2001@gmail.com>
2024-05-20 17:18:50 -0300

    Creando patch y fin de api

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { deleteUserById, getUsers } from '../db/users';
+import { deleteUserById, getUserById, getUsers } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) =>{
     try {
@@ -23,5 +23,31 @@ export const deleteUser = async ( req: express.Request, res: express.Response) =
     } catch (error) {
         console.log(error)
         return res.sendStatus(400)    
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const { username } = req.body;
+
+        if (!username) {
+            return res.sendStatus(400); // Bad Request
+        }
+
+        const user = await getUserById(id);
+
+        if (!user) {
+            return res.sendStatus(404); // Not Found
+        }
+
+        user.username = username;
+        await user.save();
+
+        return res.status(200).json(user); // OK
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500); // Internal Server Error
     }
 }
